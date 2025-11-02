e7fb8571bd0479a5a90ffabbe19c897204ae9b0a
GHOSTPF <pabloftavares460@gmai.com>
2025-11-02 11:10:35 -0300

    subida

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // 👈 Adicione esta linha (importante!)
+  base: "/", // 👈 Adicione esta linha (importante!)
   server: {
     host: "::",
     port: 8080,
